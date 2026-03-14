5aecf364d08f42e4f496975bc7d21355c93b7546
Allan Sanchez <allancasanova724@gmail.com>
2026-03-14 15:54:39 -0300

    LOOPING PRINCIPAL DA CALCULADORA

--- principal.py
+++ principal.py
@@ -1,1 +1,17 @@
+def somar(numero1, numero2):
+    res = numero1 + numero2
+    return res
 
+def executar_sistema():
+    while True:
+
+
+        opcao = int(input("numero int"))
+        if opcao == 1:
+            print("Somando...")
+            somar()
+
+            
+        if opcao == "0":
+            print("Encerrando o sistema...")
+            break
