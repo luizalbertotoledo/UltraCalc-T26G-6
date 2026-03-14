38a1204566ad1960876da5a5ecc1058a3c8d826c
Willians <willians.flavio26@gmail.com>
2026-03-14 15:29:16 -0300

    Senai_SErtaozinho

--- interface.py
+++ interface.py
@@ -1,1 +1,6 @@
+def exibir_boas_vindas(Senai_Sertaozinho):
 
+ print("==================================================")
+ print("           BEM VINDO FAÇA SEU CALCULO             ")
+ print(f"   Sistema Desenvolvido para: Senai_Sertaozinho  ")
+ print("==================================================")
