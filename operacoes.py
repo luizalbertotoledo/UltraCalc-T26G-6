46c5ad6259b2006ec2c4a96a5b4611d2e67de113
Samuel Oliveira <samueloliveirabasalha05@gmail.com>
2026-03-14 16:41:56 -0300

    Função Multiplicação

--- operacoes.py
+++ operacoes.py
@@ -1,12 +1,6 @@
-#Função Tangente
+def multiplicar(vl1, vl2):
+    res = vl1 * vl2
+    return res
 
-import math
-
-def tangente(angulo_graus):
-    angulo_radianos = math.radians(angulo_graus)
-    return math.tan(angulo_radianos)
-
-angulo = float(input("Digite o ângulo em graus: "))
-resultado = tangente(angulo)
-
-print("A tangente do ângulo é:", resultado)
+resultado = multiplicar(7, 6)
+print(resultado)
