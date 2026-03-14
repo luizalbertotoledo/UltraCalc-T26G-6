be20219b4608ff74e28bb3ce0ea2cf0123cadd41
LuisMiguel Teixeira <llmiguel1009@gmail.com>
2026-03-14 15:34:45 -0300

    Mensagem de LuisMiguel-Inserimento de Menu principal do projeto

--- interface.py
+++ interface.py
@@ -1,1 +1,12 @@
-
+def show_menu():
+    print('\n=======MENU PRINCIPAL================== ')
+    print('SELECIONE UMA OPERAÇÃO:\n')
+    print('[1].Adição.......|')
+    print('[2].Subtração....|')
+    print('[3].Multiplicação|')
+    print('[4].Divisão......|')
+    print('[5].Potenciação..|')
+    print('[0].Sair.........| ')
+    print(40*'=')
+    
+show_menu()
