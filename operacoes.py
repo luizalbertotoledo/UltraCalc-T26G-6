b003079643acc69ae6d7d8799d782707ceeb92d9
Reginaldo <naldon@live.com>
2026-03-14 16:06:54 -0300

    Mensagem - Implementar Função Exponencial

--- operacoes.py
+++ operacoes.py
@@ -1,8 +1,11 @@
+'''
 def valor_porcentagem (valor, percentual):
     res = (valor * percentual) / 100
+    return res'''
+
+import math 
+
+def exponencial(valor):
+    res = math.exp(valor)
     return res
-
-
-
-
 
