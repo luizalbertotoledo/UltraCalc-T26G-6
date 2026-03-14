ed4e9815a157a93280387159a4f31ffcc00c8066
Andre Luis <andre.savenhago>
2026-03-14 15:53:53 -0300

    Mensagem do commit

--- operacoes.py
+++ operacoes.py
@@ -1,7 +1,13 @@
 
-def somar (valor1, valor2):
-    resultado = valor1 + valor2
-    return resultado
+# def somar (valor1, valor2):
+#     resultado = valor1 + valor2
+#     return resultado
 
-resultad = somar(10, 5)
-print(resultad)
+try:
+    numero=input("digite um numero ")
+    num=int (numero)
+    print(num)
+    return num
+except ValueError:
+    print("Erro! Digite um número inteiro")
+    
