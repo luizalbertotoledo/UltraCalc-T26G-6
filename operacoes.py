5a430eb7c7b8a18025e9368ecb4327347ab75dfe
Carlos Paranhos <caduparanhos2111@gmail.com>
2026-03-14 15:40:39 -0300

    funcao divisao

--- operacoes.py
+++ operacoes.py
@@ -1,1 +1,9 @@
+from os import system as limp
+limp ("cls")
 
+numero_01 = float(input("1° numero: "))
+numero_02 = float(input("2° numero: "))
+
+resultado_01 = numero_01 / numero_02
+
+print (f"Resultado é : {resultado_01}")
