b615e8f3f54c71bad3f5656aaf73d415dca66cb0
Samuel Oliveira <samueloliveirabasalha05@gmail.com>
2026-03-14 15:56:31 -0300

    Samuel

--- operacoes.py
+++ operacoes.py
@@ -1,1 +1,19 @@
+#preciso multiplicar as pastas e separar os documentos para backup.
+from os import system as lt
+lt("cls")
 
+def multiplicar (pasta1, pasta2):
+    res = pasta1 * pasta2
+    return res
+
+resultado = multiplicar (10, 8)
+print ("Pastas Multiplicadas:")
+print(resultado)
+
+def separador_doc (doc01, doc02):
+    res = doc01 * doc02
+    return res 
+
+separacao = separador_doc (45, 7)
+print ("Documentos Separados:")
+print(separacao)
