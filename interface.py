89eb19e42badd18bc624751041061217abffb281
Daniel_Novaes <danielnovaes@ymail.com>
2026-03-16 18:28:11 -0300

    ajustado tela de resultados

--- interface.py
+++ interface.py
@@ -1,11 +1,5 @@
-def alterar_nome_empresa():
-    codigo = input("Digite o código: ")
-
-    if codigo == "8U6T":
-        novo_nome = input("Digite o novo nome: ")
-
-        with open("dados/empresa.txt", "w", encoding="utf-8") as arquivo:
-            arquivo.write(novo_nome)
-
-        print("Nome da empresa alterado com sucesso.")
-print(alterar_nome_empresa())
+def exibir_resultado(operacao, resultado):
+    print("\n==========================================")
+    print(f"Operação realizada: {operacao}")
+    print(f"Resultado: {resultado}")
+    print("==========================================\n")
