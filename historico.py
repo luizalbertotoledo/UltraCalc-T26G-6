510f0da144712bc9a6808a5ec39face3a21fc49e
Daniel Novaes <danielnovaes@ymail.com>
2026-03-21 08:59:15 -0300

    ajustado historico

--- historico.py
+++ historico.py
@@ -1,1 +1,6 @@
-
+with open("dados/historico.txt", "r", encoding="utf-8") as arquivo:
+            conteudo = arquivo.read()
+            
+            print("--- Histórico de Operações ---")
+            print(conteudo)
+            print("-------------------------------")
