7a78b82dd82759739bd5a4835dadbc7320f16ad3
Andre Luis <andre.savenhago>
2026-03-21 09:01:07 -0300

    entrada inválida

--- operacoes.py
+++ operacoes.py
@@ -1,29 +1,9 @@
-import math 
-
-def valor_porcentagem (valor, percentual):
-    res = (valor * percentual) / 100
-    return res
-
-def exponencial(valor):
-    res = math.exp(valor)
-    return res
-
-def potencia(base, expoente):
-    res = base ** expoente
-    return res
-
-def media (primeiro, segundo):
-    resul = (primeiro + segundo) /2
-    return resul
 
 # def somar (valor1, valor2):
 #     resultado = valor1 + valor2
 #     return resultado
 
-try:
-    numero=input("digite um numero ")
-    num=int (numero)
-    print(num)
-except ValueError:
-    print("Erro! Digite um número inteiro")
-    
+if opcao in ["0", "1", "2", "3", "4", "5"]:
+    print("Opção válida.")
+else:
+    print("Opção inválida. Escolha um item do menu.")    
