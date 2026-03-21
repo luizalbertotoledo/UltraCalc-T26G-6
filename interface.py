0398a0bf686e3b326d82a5c3bb699eb07399d0ba
Edilene <edilenestz.santos@gmail.com>
2026-03-21 09:03:05 -0300

    Mensagem do commit

--- interface.py
+++ interface.py
@@ -1,1 +1,11 @@
 
+
+
+
+
+def subtrair(numero1, numero2):
+    resultado = numero1 - numero2
+    ("numero1 = 125") ,("numero2 = 56")
+    return ressultado 
+    res = subtrair (125,56)
+    def exibir (resultado)
